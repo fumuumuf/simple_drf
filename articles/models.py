bad0dea3454f23178e20a1ba884f40779ae083a8
fumuumuf <kitosiro2@gmail.com>
2019-04-08 03:12:01 +0900

    [update] 更新ユーザー記録実装

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django_currentuser.middleware import get_current_authenticated_user
 
 from accounts.models import User
+
+# As model field:
+from django_currentuser.db.models import CurrentUserField
+
+
+class Foo(models.Model):
+    created_by = CurrentUserField()
 
 
 class Tag(models.Model):
@@ -8,6 +16,7 @@ class Tag(models.Model):
 
     def __str__(self):
         return f'{self.id} - {self.name}'
+
 
 class Article(models.Model):
     '''
@@ -19,6 +28,12 @@ class Article(models.Model):
     body = models.TextField('本文')
     tags = models.ManyToManyField(Tag, blank=True, related_name='articles', verbose_name='タグ', help_text='記事につけるタグ')
 
+    created_by = CurrentUserField(verbose_name='作成者', editable=False, related_name='create_articles')
+    updated_by = CurrentUserField(verbose_name='更新者', editable=False, related_name='update_articles')
+
+    def save(self, *args, **kwargs):
+        self.updated_by = get_current_authenticated_user()
+        super(Article, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.id} - {self.title}'
