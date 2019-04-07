5507a3f8ea9f92762c8dc270c0c9a7d3243f20be
fumuumuf <kitosiro2@gmail.com>
2019-04-08 01:45:07 +0900

    [add] articles app

---
+++ articles/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from accounts.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField('タグ名', max_length=120)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+class Article(models.Model):
+    '''
+    記事
+    '''
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles', verbose_name='記事の作者')
+    title = models.CharField('タイトル', max_length=120, default='no title')
+    body = models.TextField('本文')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='articles', verbose_name='タグ', help_text='記事につけるタグ')
+
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+
+
+class Meta:
+    ordering = ['-created_at']
