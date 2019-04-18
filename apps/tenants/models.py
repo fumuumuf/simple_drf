796fe13710458fce521b995ef1ca4f32f856dd1b
fumuumuf <kitosiro2@gmail.com>
2019-04-18 11:18:31 +0900

    [add] テナントモデル追加

--- apps/tenants/models.py
+++ apps/tenants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tenant(models.Model):
+    name = models.CharField('店舗名', max_length=120)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
