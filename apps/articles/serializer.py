da29619ebf29513a8ca0b4f20762a3d0eb2ecf78
fumuumuf <kitosiro2@gmail.com>
2019-06-11 08:22:15 +0900

    update

--- apps/articles/serializer.py
+++ apps/articles/serializer.py
@@ -2,17 +2,24 @@ from rest_framework import serializers
 from rest_framework.serializers import Serializer
 import copy
 
-from articles.models import Article, Category
+from articles.models import Article, Category, Tag
 
 
 class PKWritableMixin:
+    """
+    ModelSerializer を継承したフィールドを,
+    更新系アクションのときは ID(PrimaryKeyRelatedField)で更新可能とする Mixin
+    """
 
     def __init__(self, *args, **kwargs):
-        self.org_pk_fields = {}
+        self._org_pk_fields = {}
         super(PKWritableMixin, self).__init__(*args, **kwargs)
 
-    def exchange_pk_field(self, fields, name):
-        field = fields[name]
+    def _get_pk_field(self, field: serializers.ModelSerializer, many=False):
+        """
+        field(ModelSerializerの) をもとに更新用の PrimaryKeyRelatedField を生成
+        """
+
         attrs = {}
 
         # もとのフィールド定義をコピー
@@ -23,10 +30,8 @@ class PKWritableMixin:
             if hasattr(field, attr):
                 attrs[attr] = getattr(field, attr)
 
-        new_field = serializers.PrimaryKeyRelatedField(queryset=field.Meta.model._default_manager.all(),
-                                                       **attrs)
-        self.org_pk_fields[name] = field
-        fields[name] = new_field
+        return serializers.PrimaryKeyRelatedField(queryset=field.Meta.model._default_manager.all(), many=many,
+                                                  **attrs)
 
     def get_fields(self):
 
@@ -34,11 +39,19 @@ class PKWritableMixin:
         if getattr(self.context.get('view'), 'action', '') not in ('create', 'update', 'partial_update',):
             return fields
 
-        for f in [f for f in fields \
-                  if isinstance(fields[f], serializers.ModelSerializer)]:
-            if getattr(fields[f], 'read_only', True): continue
+        for f in [f for f in fields if not getattr(fields[f], 'read_only', True)]:
 
-            self.exchange_pk_field(fields, f)
+            if isinstance(fields[f], serializers.ListSerializer):
+                target_serializer = fields[f].child
+                many = True
+            else:
+                target_serializer = fields[f]
+                many = False
+
+            if isinstance(target_serializer, serializers.ModelSerializer):
+                new_field = self._get_pk_field(target_serializer, many=many)
+                self._org_pk_fields[f] = fields[f]
+                fields[f] = new_field
 
         return fields
 
@@ -46,14 +59,15 @@ class PKWritableMixin:
         res = super(PKWritableMixin, self).to_representation(obj)
 
         # 変更したフィールドはもとの serializer でレンダリング
-        for f, _serializer in self.org_pk_fields.items():
-            source = getattr(self.fields[f], 'source') or f
-            if f in res:
-                attr_val = getattr(obj, source)
-                if attr_val:
-                    res[f] = _serializer.to_representation(getattr(obj, source))
-        return res
+        for f, _serializer in self._org_pk_fields.items():
+            if f not in self.fields:
+                continue
 
+            source = getattr(self.fields[f], 'source') or f
+            if f in res and \
+                    getattr(obj, source) is not None:
+                res[f] = _serializer.to_representation(getattr(obj, source))
+        return res
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -62,11 +76,18 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
 class ArticleSerializer(PKWritableMixin, serializers.ModelSerializer):
     # category = CategorySerializer(required=False, help_text='spam')
-    alter_category = CategorySerializer(required=False, help_text='spam', source='category')
+    category = CategorySerializer(required=False, help_text='spam')
+    foos = TagSerializer(many=True, required=False, help_text='spam', source='tags')
 
     class Meta:
         model = Article
-        exclude = ('category',)
-        # fields = '__all__'
+        # exclude = ('category',)
+        fields = '__all__'
