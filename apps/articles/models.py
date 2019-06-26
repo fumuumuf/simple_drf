04fce40acda1a8e2070b56e65dfb13fc4588d420
fumuumuf <kitosiro2@gmail.com>
2019-06-26 20:29:40 +0900

    [update] ファイルアップロード可能な状態に

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,6 +1,176 @@
+import uuid
+
+import inflection
 from django.db import models
 
 from accounts.models import User
+import mimetypes
+from os.path import splitext
+
+from django.core.exceptions import ValidationError
+from django.template.defaultfilters import filesizeformat
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import ugettext_lazy as _
+
+
+@deconstructible
+class FileValidator(object):
+    """
+    ModelのFileField, ImageField用のvalidator. これらのコンストラクタの ``validator`` 引数に指定してください.
+
+    Examples:
+        usage::
+
+            MyModel(models.Model):
+                myfile = FileField(validators=FileValidator(max_bytes=24*1024*1024), ...)
+    """
+
+    _extension_message = _("Extension '%(extension)s' not allowed. Allowed extensions are: '%(allowed_extensions)s.'")
+    _mime_message = _("MIME type '%(mimetype)s' is not valid. Allowed types are: %(allowed_mimetypes)s.")
+    _min_bytes_message = _('The current file %(size)s, which is too small. The minumum file size is %(allowed_size)s.')
+    _max_bytes_message = _('The current file %(size)s, which is too large. The maximum file size is %(allowed_size)s.')
+
+    def __init__(self,
+                 max_bytes=None,
+                 min_bytes=0,
+                 allowed_mimetypes=None,
+                 allowed_extensions=None, *args, **kwargs):
+        """
+
+        Args:
+            max_bytes: 最小ファイルサイズ(bytes)
+            min_bytes: 最大ファイルサイズ(bytes)
+            allowed_mimetypes: 許可するmimetypeリスト ie. ('txt', 'doc')
+            allowed_extensions: 許可する拡張子リスト ie. ('image/png', )
+            *args:
+            **kwargs:
+        """
+        self.allowed_extensions = allowed_extensions
+        self.allowed_mimetypes = allowed_mimetypes
+        self.min_bytes = min_bytes
+        self.max_bytes = max_bytes
+
+    def __call__(self, value):
+        """
+        Check the extension, content type and file size.
+        """
+
+        # Check the extension
+        ext = splitext(value.name)[1][1:].lower()
+        if self.allowed_extensions and ext not in self.allowed_extensions:
+            message = self._extension_message % {
+                'extension': ext,
+                'allowed_extensions': ', '.join(self.allowed_extensions)
+            }
+
+            raise ValidationError(message)
+
+        # Check the content type
+        mimetype = mimetypes.guess_type(value.name)[0]
+        if self.allowed_mimetypes and mimetype not in self.allowed_mimetypes:
+            message = self._mime_message % {
+                'mimetype': mimetype,
+                'allowed_mimetypes': ', '.join(self.allowed_mimetypes)
+            }
+
+            raise ValidationError(message)
+
+        # Check the file size
+        filesize = len(value)
+        if self.max_bytes and filesize > self.max_bytes:
+            message = self._max_bytes_message % {
+                'size': filesizeformat(filesize),
+                'allowed_size': filesizeformat(self.max_bytes)
+            }
+
+            raise ValidationError(message)
+
+        elif filesize < self.min_bytes:
+            message = self._min_bytes_message % {
+                'size': filesizeformat(filesize),
+                'allowed_size': filesizeformat(self.min_bytes)
+            }
+
+            raise ValidationError(message)
+
+    def __eq__(self, other):
+        """
+        validation内容を比較
+
+        Args:
+            other: FileValidator
+
+        Returns:
+            bool: 比較結果
+        """
+
+        def compare_list(self_list, other_list):
+            _self = set(self_list) if self_list else set()
+            _other = set(other_list) if other_list else set()
+            return _self == _other
+
+        return (compare_list(self.allowed_extensions, other.allowed_extensions) and
+                compare_list(self.allowed_mimetypes, other.allowed_mimetypes) and
+                self.min_bytes == other.min_bytes and
+                self.max_bytes == other.max_bytes)
+
+
+@deconstructible
+class MyCharValidator(object):
+    def __call__(self, value):
+        """
+        Check the extension, content type and file size.
+        """
+
+        # Check the extension
+        if not value:
+            return
+        if len(value) > 0:
+            raise ValidationError('spam')
+
+    def __eq__(self, other):
+        """
+        validation内容を比較
+
+        Args:
+            other: FileValidator
+
+        Returns:
+            bool: 比較結果
+        """
+
+        def compare_list(self_list, other_list):
+            _self = set(self_list) if self_list else set()
+            _other = set(other_list) if other_list else set()
+            return _self == _other
+
+        return (compare_list(self.allowed_extensions, other.allowed_extensions) and
+                compare_list(self.allowed_mimetypes, other.allowed_mimetypes) and
+                self.min_bytes == other.min_bytes and
+                self.max_bytes == other.max_bytes)
+
+
+def upload_to_uuid4_name(instance, filename):
+    """
+    uuid4をファイル名としたアップロード先を取得する関数
+
+    モデルの FileFieldやImageFieldのupload_toに指定してください.
+
+    Examples:
+        usages::
+
+            class LiquorImage(BaseModel):
+                # ImageField の upload_to に指定する
+
+
+    """
+    extension = filename.split('.')[-1]
+
+    _uuid = str(uuid.uuid4()).replace('-', '_')
+    return '{}/{}/{}.{}'.format(instance._meta.app_label,
+                                inflection.underscore(instance.__class__.__name__),
+                                _uuid,
+                                extension)
 
 
 class Tag(models.Model):
@@ -9,16 +179,24 @@ class Tag(models.Model):
     def __str__(self):
         return f'{self.id} - {self.name}'
 
+
 class Article(models.Model):
     '''
     記事
     '''
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles', verbose_name='記事の作者')
-    title = models.CharField('タイトル', max_length=120, default='no title')
+    title = models.CharField('タイトル', max_length=120, validators=[MyCharValidator()],
+                             default='no title')
     body = models.TextField('本文')
     tags = models.ManyToManyField(Tag, blank=True, related_name='articles', verbose_name='タグ', help_text='記事につけるタグ')
 
+    file = models.FileField('file', null=True, blank=True, upload_to=upload_to_uuid4_name,
+                            validators=[
+                                FileValidator(max_bytes=3, allowed_extensions=('pdf', 'jpeg', 'jpg', 'png'))],
+                            help_text='アップロード可能な最大ファイルサイズは3Bです. '
+                                      '拡張子は次のいずれかが使用できます {"pdf", "jpeg", "jpg", "png"}.',
+                            )
 
     def __str__(self):
         return f'{self.id} - {self.title}'
--- apps/articles/serializer.py
+++ apps/articles/serializer.py
@@ -1,9 +1,71 @@
+import base64
+import binascii
+import uuid
+
+from django.core.files.base import ContentFile
 from rest_framework import serializers
+from rest_framework.fields import FileField
 
 from articles.models import Article
+from django.db import models
 
+from accounts.models import User
+import mimetypes
+from os.path import splitext
+
+from django.core.exceptions import ValidationError
+from django.template.defaultfilters import filesizeformat
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import ugettext_lazy as _
+
+class Base64FileField(serializers.FileField):
+    """
+    base64でエンコードされたファイルを扱うField
+    base64 エンコードされたファイルをアップロードする場合はこの Field を使用する.
+
+    References:
+        https://stackoverflow.com/questions/28036404/django-rest-framework-upload-image-the-submitted-data-was-not-a-file
+    Notes:
+        Heavily based on
+        https://github.com/tomchristie/django-rest-framework/pull/1268
+
+        Updated for Django REST framework 3.
+
+        and based on
+        https://github.com/tsh/django-rest-framework-base64-fields
+    """
+
+    _ERROR_MESSAGE = 'Base64データが不正です.'
+
+    _MIME_MAPPING = {
+        'image/jpeg': '.jpg',
+        'application/pdf': '.pdf',
+        'image/png': '.png'
+    }
+
+    def to_internal_value(self, data):
+        return super(Base64FileField, self).to_internal_value(data)
+        # if not isinstance(data, str):
+        #     raise serializers.ValidationError(self._ERROR_MESSAGE)
+        #
+        # split_data = data.replace('data:', '', 1).split(';base64,')
+        # if len(split_data) != 2:
+        #     raise serializers.ValidationError(self._ERROR_message + ' データの中に MIME Type が含まれているか確認してください.')
+        #
+        # mime, encoded_data = split_data
+        # try:
+        #     extension = self._MIME_MAPPING[mime] if mime in self._MIME_MAPPING.keys() else mimetypes.guess_extension(
+        #         mime)
+        #
+        #     data = ContentFile(base64.b64decode(encoded_data),
+        #                        name='{name}{extension}'.format(name=str(uuid.uuid4()), extension=extension))
+        #     return super(Base64FileField, self).to_internal_value(data)
+        #
+        # except (ValueError, binascii.Error):
+        #     raise serializers.ValidationError(self._ERROR_MESSAGE)
 
 class ArticleSerializer(serializers.ModelSerializer):
+    file = FileField(allow_null=True, write_only=True, help_text='このフィールドは file 登録用です.')
     class Meta:
         model = Article
         fields = '__all__'
