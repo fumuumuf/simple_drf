d9e3de041fb2d0f10775a3dfb9d739a1bafac6ce
fumuumuf <kitosiro2@gmail.com>
2019-07-08 09:17:15 +0900

    update serializer

--- apps/articles/serializer.py
+++ apps/articles/serializer.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
+from rest_framework.utils.model_meta import get_field_info
 
-from articles.models import Article
+from articles.models import Article, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+        # many to many がないならこれでもOK
+        # read_only_fields = [f.name for f in Article._meta.fields
+        #                     if f.name != 'category']
+
+        _info = get_field_info(Article)
+        read_only_fields = [f for f in _info.fields.keys()]
+        read_only_fields += [f for f in _info.forward_relations.keys() if f != 'category']
