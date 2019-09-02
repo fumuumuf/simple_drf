4ac6c7ccdd459f69b3494c4a66d868458231c2da
fumuumuf <kitosiro2@gmail.com>
2019-09-03 08:40:52 +0900

    category 周りの設定追加

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from articles.models import Article, Tag
+from articles.models import Article, Tag, Category
 
 admin.site.register(Article)
 admin.site.register(Tag)
+admin.site.register(Category)
--- apps/articles/serializer.py
+++ apps/articles/serializer.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from articles.models import Article
+from articles.models import Article, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    def __init__(self,*args,**kwargs):
+        super(CategorySerializer, self).__init__(*args,**kwargs)
+        print(self.context)
+
+    class Meta:
+        model = Category
+        # fields = '__all__'
+
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = Article
         fields = '__all__'
