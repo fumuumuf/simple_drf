ad6d4c69ca65ce731cd84a09068b01f53419d121
fumuumuf <kitosiro2@gmail.com>
2019-09-02 16:50:35 +0900

    登録, 更新用にカスタマイズ

--- apps/articles/serializer.py
+++ apps/articles/serializer.py
@@ -1,4 +1,4 @@
-from drf_writable_nested import NestedUpdateMixin
+from drf_writable_nested import NestedUpdateMixin, NestedCreateMixin
 from rest_framework import serializers
 from rest_framework.utils.model_meta import get_field_info
 
@@ -11,16 +11,17 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ArticleSerializer(NestedUpdateMixin, serializers.ModelSerializer):
-    category = CategorySerializer(many=False)
+class ArticleSerializer(NestedCreateMixin, NestedUpdateMixin, serializers.ModelSerializer):
+    category = CategorySerializer(many=False, required=False)
     class Meta:
         model = Article
         fields = '__all__'
 
+        # まとめて readonly 化
         # many to many がないならこれでもOK
         # read_only_fields = [f.name for f in Article._meta.fields
         #                     if f.name != 'category']
 
         _info = get_field_info(Article)
-        read_only_fields = [f for f in _info.fields.keys()]
-        read_only_fields += [f for f in _info.forward_relations.keys() if f != 'category']
+        # read_only_fields = [f for f in _info.fields.keys()]
+        # read_only_fields += [f for f in _info.forward_relations.keys() if f != 'category']
