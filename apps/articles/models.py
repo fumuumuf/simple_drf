0554f818a1ccaaf3e106c6e7756bc0f7b5bbe470
fumuumuf <kitosiro2@gmail.com>
2019-11-02 14:53:02 +0900

    smallfix

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -15,9 +15,9 @@ class Category(models.Model):
 
 
 class Article(models.Model):
-    '''
+    """
     記事
-    '''
+    """
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles', verbose_name='記事の作者')
     title = models.CharField('タイトル', max_length=120, default='no title')
@@ -27,7 +27,3 @@ class Article(models.Model):
 
     def __str__(self):
         return f'{self.id} - {self.title}'
-
-
-class Meta:
-    ordering = ['-created_at']
--- apps/articles/serializer.py
+++ apps/articles/serializer.py
@@ -4,18 +4,14 @@ from articles.models import Article, Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    def __init__(self,*args,**kwargs):
-        super(CategorySerializer, self).__init__(*args,**kwargs)
-        print(self.context)
-
     class Meta:
         model = Category
         fields = '__all__'
 
 
-
 class ArticleSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
+
     class Meta:
         model = Article
         fields = '__all__'
