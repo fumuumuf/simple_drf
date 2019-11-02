5a1ee44c1f581b4c9fd63b5e75e714b95074c95f
fumuumuf <kitosiro2@gmail.com>
2019-11-02 16:25:12 +0900

    設定更新

--- apps/simple_drf/settings.py
+++ apps/simple_drf/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
 }
 
 # silk 有効化
-SILKY_PYTHON_PROFILER = True
+SILKY_PYTHON_PROFILER = False
 
 # django_extensions settings
 # `python manage.py shell_plus --notebook`でjupyter notebook起動
@@ -157,8 +157,70 @@ NOTEBOOK_ARGUMENTS = [
     '--ip', '0.0.0.0',
     '--port', '8000',
     '--allow-root',
-    '--no-browser'
+    '--no-browser',
+    "--NotebookApp.token=''",
 ]
+SHELL_PLUS_PRINT_SQL = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'site_media/')
 MEDIA_URL = '/media/'
+
+# # Logging
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'django.server': {
+#             '()': 'django.utils.log.ServerFormatter',
+#             'format': '[%(server_time)s] %(message)s',
+#         },
+#         'verbose': {
+#             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+#         },
+#         'simple': {
+#             'format': '%(levelname)s %(message)s'
+#         },
+#     },
+#     'filters': {
+#         'require_debug_true': {
+#             '()': 'django.utils.log.RequireDebugTrue',
+#         },
+#     },
+#     'handlers': {
+#         'django.server': {
+#             'level': 'INFO',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'django.server',
+#         },
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'simple'
+#         },
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'class': 'django.utils.log.AdminEmailHandler'
+#         }
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console'],
+#             'propagate': True,
+#         },
+#         'django.server': {
+#             'handlers': ['django.server'],
+#             'level': 'INFO',
+#             'propagate': False,
+#         },
+#         'django.request': {
+#             'handlers': ['mail_admins', 'console'],
+#             'level': 'ERROR',
+#             'propagate': False,
+#         },
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG'
+#         },
+#     }
+# }
+#
