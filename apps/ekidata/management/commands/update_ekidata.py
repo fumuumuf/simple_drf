db2235fa32c1161330d31bc9b0d0580beb4e5ad6
fumuumuf <kitosiro2@gmail.com>
2019-12-04 08:58:36 +0900

    station 取得の修正

--- apps/ekidata/management/commands/update_ekidata.py
+++ apps/ekidata/management/commands/update_ekidata.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.core.management import BaseCommand
 from rest_framework import serializers
 
@@ -8,8 +10,8 @@ from ekidata.models import Line, Station
 class LineSerializer(serializers.ModelSerializer):
     line_cd = serializers.IntegerField(source='id')
     line_name = serializers.CharField(source='name')
-    line_lat = serializers.CharField(source='lat', allow_blank=True, required=False)
-    line_lon = serializers.CharField(source='lon', allow_blank=True, required=False)
+    line_lat = serializers.DecimalField(source='lat', required=False, allow_null=True, max_digits=9, decimal_places=6)
+    line_lon = serializers.DecimalField(source='lon', required=False, allow_null=True, max_digits=9, decimal_places=6)
     line_zoom = serializers.CharField(source='zoom', allow_blank=True, required=False)
 
     class Meta:
@@ -29,6 +31,15 @@ class StationSerializer(serializers.ModelSerializer):
         model = Station
         exclude = ['id', 'name', 'prefecture']
 
+
+def round_loc(v):
+    if not v: return v
+    v = str(v)
+    l = 9
+    if '.' in v:
+        l = min(v.index('.') + 7, l + 1)
+    return Decimal(v[:l])
+
 class Command(BaseCommand):
     help = 'load line data'
     station_cds = set()
@@ -47,6 +58,8 @@ class Command(BaseCommand):
         for cd in all_line:
             line_data = self.api.get_line(cd)
             line = line_data['line']
+            line['line_lon'] = round_loc(line.get('line_lon', None))
+            line['line_lat'] = round_loc(line.get('line_lat', None))
 
             print('load', cd, line['line_name'])
             instance = Line.objects.filter(id=int(line['line_cd'])).first()
@@ -68,21 +81,24 @@ class Command(BaseCommand):
 
             instance = Station.objects.filter(id=int(cd)).first()
             if instance:
+                # HACK: parameter で再取得するかどうかを制御できるように
                 # すでに登録済の場合は pass
                 # serializer = StationSerializer(instance, data=station, partial=True)
                 continue
 
             station = self.api.get_station(cd)
             print('load station', cd, station['station_name'])
+
+            station['lon'] = round_loc(station.get('lon', None))
+            station['lat'] = round_loc(station.get('lat', None))
+
             serializer = StationSerializer(data=station)
 
             if serializer.is_valid(raise_exception=False):
                 serializer.save()
             else:
                 print('load station error', cd, station['station_name'])
-                print(serializers.errors)
-
-
+                print(serializer.errors)
 
     def handle(self, *args, **options):
         self.api = EkidataAPI()
